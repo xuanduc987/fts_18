078a7b1b2ca72be0299ecbf45c979dd86e2b7e4a
Duc Nghiem Xuan <nghiem.xuan.duc@framgia.com>
2015-03-18 16:18:23 +0700

    Edit css for signup, signin, edit user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,16 @@
 module ApplicationHelper
-  def full_title(page_title = '')
+  def full_title page_title = ''
     base_title = 'Learning System'
     if page_title.empty?
       base_title
     else
       "#{page_title} | #{base_title}"
     end
+  end
+
+  def alert_type message_type
+    dictionary = {alert: "danger", notice: "info"}
+    dictionary.default = message_type
+    dictionary[message_type.to_sym]
   end
 end
