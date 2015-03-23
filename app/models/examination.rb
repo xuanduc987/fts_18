163ed6930b9ce5cd9f3fa074d5a27552e8d2bcf3
Duc Nghiem Xuan <nghiem.xuan.duc@framgia.com>
2015-03-23 11:43:02 +0700

    Allow save answers within time limit

--- app/models/examination.rb
+++ app/models/examination.rb
@@ -1,7 +1,15 @@
 class Examination < ActiveRecord::Base
+  TIME_LIMIT = 20
+
   belongs_to :course
   belongs_to :user
   has_many :answers, dependent: :destroy
 
+  accepts_nested_attributes_for :answers
+
   delegate :name, to: :course, prefix: true
+
+  def time_up?
+    created_at < TIME_LIMIT.minutes.ago
+  end
 end
